d4ef11795de5421ad469b94160904cc4ca4e1ab4
shreya-mishra <shreyamishra040398@gmail.com>
2024-01-23 16:34:39 +0530

    login flow

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View>
+      {/* <loginscreen>
+        <form>
+          <input />
+          <input />
+          <button />
+        </form>
+      </loginscreen>
+<stack>
+if login ==> then successscreen
+      if not ===> then loginscreen
+
+</stack> */}
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
