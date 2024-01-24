462b732977627b35177c03c4d4509cf25733a1d0
shreya-mishra <shreyamishra040398@gmail.com>
2024-01-24 14:24:18 +0530

    added screens

---
+++ src/screen/LoginScreen.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import UserInput from "../component/UserInput";
+import SubmitButton from "../component/SubmitButton";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      {/* <UserInput />
+      <UserInput />
+      <SubmitButton /> */}
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screen/SuccessScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SuccessScreen = () => {
+  return (
+    <View>
+      <Text>SuccessScreen</Text>
+    </View>
+  );
+};
+
+export default SuccessScreen;
+
+const styles = StyleSheet.create({});
